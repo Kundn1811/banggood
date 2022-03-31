b5bdd76fba6ccdb022a8a86be0226575bc1b131d
Geetu Rana <geeturana07@gmail.com>
2022-03-31 10:36:01 +0530

    Electronis page added

---
+++ electronics.js
@@ -0,0 +1,523 @@
+var electronicData=[
+    {
+        imgUrl: "https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/B3/10/afb268bb-95b0-46b3-9906-456b82ba25ed.jpg.webp",
+        price:40.09,
+        curruncy:"US$",
+        strikedoffprice:"US$40.09",
+        discount:"",
+        name:"RUIZU M7 2.8 Inch Screen 8GB",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/5E/65/eaefeee8-7384-4689-9e48-f8007ffa5daf.jpg.webp",
+        price:39.40,
+        curruncy:"US$",
+        strikedoffprice:"US$68.99",
+        discount:"42% Off",
+        name:"H96 Max V11 RK3318 DDR3 4GB",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/25/3C/12cbccca-34bb-4b76-802f-9d8a11ca1160.jpg.webp",
+        price: 40.99,
+        curruncy:"US$",
+        strikedoffprice:"US$49.99",
+        discount:"18% Off",
+        name:"4K Full HD 1080P 24MP 18X Zoom 3",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/B4/7C/8b5d1473-7564-43ff-a629-565fa3df6366.jpg.webp",
+        price: 129.99,
+        curruncy:"US$",
+        strikedoffprice:"US$302.99",
+        discount:"57% Off",
+        name:"LOKAMAT APPLLP6 1.6inch",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/E7/C0/b8834a2f-762a-4cfe-ab6f-eef19e7cee70.jpg.webp",
+        price: 99.99,
+        curruncy:"US$",
+        strikedoffprice:"US$116.99",
+        discount:"15% Off",
+        name:"Hohem iSteady Pro 4 bluetooth 3-"
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/ser1/banggood/images/C0/B3/4a19158e-4557-4bd7-ab2d-4c16a24ff08a.jpg.webp",
+        price: "34.99",
+        curruncy:"US$",
+        strikedoffprice:"US$41.99",
+        discount:"17% Off",
+        name:"Flydigi Q1 Keyboard Mouse",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/ser1/banggood/images/A1/B3/be98f353-b582-45cc-a857-1f92d63af455.jpg.webp",
+        price:98.29,
+        curruncy:"US$",
+        strikedoffprice:"US$120.20",
+        discount:"18% Off",
+        name:"4Pc BAOFENG UV-5R DUAL BAND",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/39/82/d47cdf2b-b773-4742-a60b-fec1e47b6df3.jpg.webp",
+        price:99.99 ,
+        curruncy:"US$",
+        strikedoffprice:"US$106.99",
+        discount:"20% Off",
+        name:"BlitzWolf® BW-BE1 1.6 inch HD",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/C0/C0/1f1bba9e-9d67-4070-83b1-19141faf16c4.jpg.webp",
+        price:85.99,
+        curruncy:"US$",
+        strikedoffprice:"us$106.99",
+        discount:"20%OFF",
+        name:"XIAOMI Smart Outdoor Security",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/3E/8E/8d3c66b4-83f5-42dc-863b-5a65830275c5.jpg.webp",
+        price:89.99,
+        curruncy:"US$",
+        strikedoffprice:"US$199.99",
+        discount:"55%",
+        name:"Rogbid Air 1.75 inch HD Screen Dual",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/92/1F/6b022593-0734-479e-9152-7cdc122c511c.jpg.webp",
+        price:50.59,
+        curruncy:"US$",
+        strikedoffprice:"US$83.69",
+        discount:"40%",
+        name:"Smart BMS 1A 2A 6A Balance",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/FA/8A/fc5eeb55-b2ba-40a3-a22e-600f8c4b928f.jpg.webp",
+        price:78.99,
+        curruncy:"US$",
+        strikedoffprice:"US$153.99",
+        discount:"49% Off",
+        name:"Gamesir X2 Stretchable Bluetooth",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/B3/10/afb268bb-95b0-46b3-9906-456b82ba25ed.jpg.webp",
+        price: 40.09,
+        curruncy:"US$",
+        strikedoffprice: "US$40.09",
+        discount: '',
+        name:" RUIZU M7 2.8 Inch Screen 8GB",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/4A/C3/3147fba3-0986-4e28-828f-77488bfb3b5d.jpg.webp",
+        price: 36.83,
+        curruncy:"US$",
+        strikedoffprice: "US$49.99",
+        discount: '26% Off',
+        name:"Karaoke Wireless Microphone",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/ED/18/dc2a3969-9f41-4f58-a1ef-37d22ff4bcf0.jpg.webp",
+        price:35.99,
+        curruncy:"US$",
+        strikedoffprice: "US$62.99",
+        discount: '43%Off',
+        name:"[50 Days Standby]Kospet Rock 1.69",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/14/2A/cee9bb1a-cbc1-4676-a2b9-4ecd699fada4.jpg.webp",
+        price: 35.99,
+        curruncy:"US$",
+        strikedoffprice: "US$63.99",
+        discount: '44%Off',
+        name:"H96 Max V11 RK3318 DDR3 4GB",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/DD/22/6637175b-c49e-4e3f-98c3-af4461af1fb8.jpg.webp",
+        price:22.99,
+        curruncy:"US$",
+        strikedoffprice: "US$47.99",
+        discount: '52%',
+        name:"BIGBIG WON R100 Pro Wireless",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/24/EF/d264b16d-628f-4566-bb79-f4cf5184303a.jpg.webp",
+        price: 36.99,
+        curruncy:"US$",
+        strikedoffprice: "US$69.99",
+        discount: '47%',
+        name:"[Global Version]Xiaomi Mi Band 6",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/AC/99/da08bbf2-16c0-4645-aeef-6c3a1948758b.jpg.webp",
+        price:32.99 ,
+        curruncy:"US$",
+        strikedoffprice: "US$39.99",
+        discount: '18% Off',
+        name:"GTMEDIA V8 Finder BT05 1080p",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/CA/7C/d46f1acb-9491-4d37-8151-687a6bdbb1c6.jpg.webp",
+        price: 33.99 ,
+        curruncy:"US$",
+        strikedoffprice: "US$59.99",
+        discount: '43%Off',
+        name:"Bakeey KY TWS Blutooth 5.0 Smart",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/F1/3A/b8d4b0f4-aa8b-4861-a76d-1b127fab2231.jpg.webp",
+        price: 27.99,
+        curruncy:"US$",
+        strikedoffprice: "US$59.99",
+        discount: '53% Off',
+        name:"New Lenovo QT81 TWS bluetooth",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/ED/11/547382c6-4a0a-4a24-9834-67f69c5e6d46.jpg.webp",
+        price: 21.79,
+        curruncy:"US$",
+        strikedoffprice: "US$29.99",
+        discount: '27% Off',
+        name:"SONOFF IFan03 AC100-240V",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/BD/CF/c1587418-354b-449f-978d-3bfb4d776cb9.jpg.webp",
+        price: 39.99,
+        curruncy:"US$",
+        strikedoffprice: "US$69.99",
+        discount: '43% Off',
+        name:"BlitzWolf BW-BTS6 blutoth 5.0",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/07/75/157f1b8e-aa72-4a56-9084-78b9061bc3ad.jpg.webp",
+        price: 26.99,
+        curruncy:"US$",
+        strikedoffprice: "US$35.99",
+        discount: '25% Off',
+        name:"bluetooth 5.0 MP3 Audio Decoder",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/6D/81/ee5de7a8-6503-4f7d-b04b-0c125d9a70e3.jpg.webp",
+        price:26.76 ,
+        curruncy:"US$",
+        strikedoffprice: "US$37.99",
+        discount: '30% Off',
+        name:"Kl-180 1200W Car Audio Power",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/69/19/0fa72ab4-5bcf-4ab2-8ccb-d626e8df9ba3.jpg.webp",
+        price:29.99,
+        curruncy:"US$",
+        strikedoffprice: "US$59.99",
+        discount: '50% Off',
+        name:"BlitzWolf BW-FLB5 TWS blutooth",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/3C/1A/d6829906-b1e9-4895-b796-23256d6f9d32.jpg.webp",
+        price: 42.99,
+        curruncy:"US$",
+        strikedoffprice: "US$64.79",
+        discount: '34%',
+        name:"Ulanzi VL110 Portable RGB LED",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/B7/82/8ac9868a-9ce0-42fd-94f2-41b7cc467867.jpg.webp",
+        price:18.99 ,
+        curruncy:"US$",
+        strikedoffprice: "US$26.99",
+        discount: '30%',
+        name:"PLEXTONE GS1 3 in 1 Sound Card",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/C0/93/04e0f71e-47fe-4cac-9e8c-727bdf389ebf.jpg.webp",
+        price: 18.99,
+        curruncy:"US$",
+        strikedoffprice: "US$29.99",
+        discount: '37% Off',
+        name:"QCC3008 DC12V 2A Home Audio",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/5C/96/dd80ead0-df54-44c1-8fe0-ce3f49dbec65.jpg.webp",
+        price: 29.99,
+        curruncy:"US$",
+        strikedoffprice: "US$59.99",
+        discount: '50% Off',
+        name:"Nokia E3101 TWS bluetooth 5.1",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/71/4F/88d9e64d-6514-491f-9bbc-cbb723c7bc24.jpg.webp",
+        price: 34.94,
+        curruncy:"US$",
+        strikedoffprice: "US$45.99",
+        discount: '24% Off',
+        name:"Bakeey L18 Milanese 24-hour Heart",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/03/5B/bd0f503f-74c4-41a1-87d4-042339146293.jpg.webp",
+        price: 19.99,
+        curruncy:"US$",
+        strikedoffprice: "US$26.99",
+        discount: '26% Off',
+        name:"Lenovo QT81 TWS bluetooth 5.0",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/ser1/banggood/images/33/28/ad92c029-c52e-41b6-9db7-d2d551726014.JPG.webp",
+        price: 10.79,
+        curruncy:"US$",
+        strikedoffprice: "US$20.99",
+        discount: '49% Off',
+        name:"Digital LCD Dual Channel FM Stereo",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/B0/68/483e339f-ade4-4af0-9c51-8bd42c0dc7d1.jpg.webp",
+        price:9.99 ,
+        curruncy:"US$",
+        strikedoffprice: "US$19.99",
+        discount: '50% Off',
+        name:"Bakeey BTS-06 bluetooth Speaker",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/9B/0F/c7178416-2fa1-4c62-9aa6-5099b0baab2b.jpg.webp",
+        price: 7.99,
+        curruncy:"US$",
+        strikedoffprice: "US$6.99",
+        discount: '',
+        name:"TPA3116 50W*2+100W bluetooth",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/7C/41/b5728bde-e447-4ffb-ac39-9820729e3092.jpg.webp",
+        price: 7.66,
+        curruncy:"US$",
+        strikedoffprice: "US$12.89",
+        discount: '41% Off',
+        name:"AI Smart Video 1080P Full HD Web",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/EB/7C/a058b32b-4c1d-477d-a04d-54f971f73b48.jpg.webp",
+        price: 5.65,
+        curruncy:"US$",
+        strikedoffprice: "US$5.65",
+        discount: '',
+        name:"DC5-32V RF 433MHz Ewelink Tuya",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/D1/D5/f23f1514-db96-4bd5-adbc-bf9224f7adb3.JPG.webp",
+        price:0.34 ,
+        curruncy:"US$",
+        strikedoffprice: "US$8.99",
+        discount: '96%',
+        name:"Gas Mask Dust Mask Filter",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/71/12/d560121d-a349-4f35-a2fb-83e9fe2611b3.jpg.webp",
+        price: 459.00,
+        curruncy:"US$",
+        strikedoffprice: "US$509.00",
+        discount: '10% Off',
+        name:"FLSUN® Super Racer(SR) 3D Printer",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/8D/7D/0aca3bf7-3ec9-44c5-afe6-cea61fb15faf.png.webp",
+        price: 389.00,
+        curruncy:"US$",
+        strikedoffprice: "US$523.69",
+        discount: '26% Off',
+        name:"Creality 3D® Ender-3 S1 3D Printer",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/4E/A8/83444125-ee77-491d-9c19-6cf9e3438394.jpg.webp",
+        price: 279.95,
+        curruncy:"US$",
+        strikedoffprice: "US$341.19",
+        discount: '18% Off',
+        name:"Creality 3D® Ender-3 V2 Upgraded",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/18/0F/c78eab87-d57e-4fd2-a594-6682c068aba9.jpg.webp",
+        price: 418.95,
+        curruncy:"US$",
+        strikedoffprice: "US$709.99",
+        discount: '41% Off',
+        name:"Artillery® Sidewinder X2 &",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/27/FC/039bc230-0113-48c8-91f4-8c1e4bd5fb31.jpg.webp",
+        price: 669.29,
+        curruncy:"US$",
+        strikedoffprice: "US$1,029.65",
+        discount: '35% Off',
+        name:"Growatt Hybr1d Solar Inverter",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/85/A8/4f80d6ee-dfd3-46c1-96f9-f493a49ab004.jpg.webp",
+        price:119.99 ,
+        curruncy:"US$",
+        strikedoffprice: "US$189.99",
+        discount: '37% Off',
+        name:"BlitzWolf® BW-WA3 100W",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/9D/9B/12fa5226-9825-4e8c-aac2-016dde53068e.jpg.webp",
+        price: 13.95,
+        curruncy:"US$",
+        strikedoffprice: "US$567.57",
+        discount: '45% Off',
+        name:"Artillery® GeniusPro & Genius 3D",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/2B/13/14b20017-2f76-4887-8ad4-a69dc30bd3b0.jpg.webp",
+        price: 489.00,
+        curruncy:"US$",
+        strikedoffprice: "US$789.63",
+        discount: '8% Off',
+        name:"Creality 3D® Ender-3 S1 pro 3D",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/32/83/fe953408-9e6f-4b95-88e3-8a47f3f4bf8c.jpg.webp",
+        price: 9.99,
+        curruncy:"US$",
+        strikedoffprice: "US$99.99",
+        discount: '50% Off',
+        name:"Xiaomi Redmi Smart Band Pro 1.47",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/E9/03/dfb0f178-c4a7-41cb-89e0-a014271d707e.jpg.webp",
+        price:572.29 ,
+        curruncy:"US$",
+        strikedoffprice: "US$745.65",
+        discount: '23% Off',
+        name:"PowMr 5.5KW 5.5KVA Solar Inverter",
+    },
+    {
+        imgUrl:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/BA/8E/4572731c-ff1f-4a86-bf74-4f754ef0a06e.jpg.webp",
+        price: 56.99,
+        curruncy:"US$",
+        strikedoffprice: "US$159.99",
+        discount: '64% Off',
+        name:"BlitzWolf® BW-ANC5 bluetooth 5.0",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/38/6A/a54a0b26-ed12-4aa2-823c-69b4d52cf664.jpg.webp",
+        curruncy:"US$",
+        price:37.99 ,
+        strikedoffprice: "",
+        discount: '37% Off',
+        name:"2021 BaoFeng UV-S9 Plus Walkie",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/65/FE/ecbb5bfd-f245-4c47-88bb-abae79a38a05.jpg.webp",
+        price: 109.99,
+        curruncy:"US$",
+        strikedoffprice: "US$599.99",
+        discount: '82% Off',
+        name:"BlitzWolf® AirAux AA-SAR2 80W",
+    },
+    {
+        imgUrl:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/C5/24/bca3e3b0-abba-4e1e-aeaf-6b4d70c1b72b.jpg.webp",
+        price:699.00 ,
+        curruncy:"US$",
+        strikedoffprice: "US$1,132.00",
+        discount: '38% Off',
+        name:"Creality 3D® CR-10 Smart Pro 3D",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/BF/24/14e594bd-5716-4849-8101-29e997c6c3b2.jpg.webp",
+        price: 1279.99,
+        curruncy:"US$",
+        strikedoffprice: "US$1,644.87",
+        discount: '22% Off',
+        name:"ONE-NETBOOK OnexPlayer 1S AMD",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/65/8D/acd0bd51-5fdf-4a50-b71c-cfac35fd1e4b.jpg.webp",
+        price: 42.99,
+        curruncy:"US$",
+        strikedoffprice: "US$59.99",
+        discount: '28% Off',
+        name:"2022 BAOFENG UV-S9 Plus Walkie",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/D2/13/4dcfbdeb-1acb-4c32-a7b7-a6b8f654f800.jpg.webp",
+        price: 40.79,
+        curruncy:"US$",
+        strikedoffprice: "US$59.99",
+        discount: '32% Off',
+        name:"Moeshouse Tuya ZigBee3.0 Smart",
+    },
+    {
+        imgUrl:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/6F/6F/71c2eeff-2714-42c7-b8bd-cd0cea960fb3.jpg.webp",
+        price: 121.01,
+        curruncy:"US$",
+        strikedoffprice: "US$169.99",
+        discount: '29% Off',
+        name:"Xiaomi TV Sound Bar Speaker",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/BF/6E/e61b7766-277b-4693-88c8-41be80b8ac35.png.webp",
+        price: 58.99,
+        curruncy:"US$",
+        strikedoffprice: "US$129.99",
+        discount: '55% Off',
+        name:"Xiaomi Redmi Watch 2 Lite 1.5 inch",
+    },
+    {
+        imgUrl:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/31/5E/51dd0df5-dc19-489b-89ac-a3459258d779.jpg.webp",
+        price:899.00 ,
+        curruncy:"US$",
+        strikedoffprice: "US$1,399.99",
+        discount: '36% Off',
+        name:"QIDI® X-Plus Large Size Pre-",
+    },
+    
+   
+];
+
+var Discription=JSON.parse(localStorage.getItem("discription"))||[];
+electronicData.map(function (elem){
+    var box=document.createElement("div");
+     box.setAttribute("id","inner");
+
+     var img=document.createElement("img");
+     img.style.cursor="pointer";
+     img.setAttribute("class","img")
+     img.addEventListener("click", function() {
+         Discription.push(elem);
+         localStorage.setItem("discription",JSON.stringify(Discription));
+     })
+     img.src=elem.imgUrl;
+
+    var crcy=document.createElement("h3");
+     crcy.innerText=elem.curruncy;
+    
+    var prc=document.createElement("h3");
+     prc.innerText=elem.price;
+    var box2=document.createElement("div");
+    box2.setAttribute("id","prc")
+    box2.append(crcy,prc)
+
+    var sop=document.createElement("p");
+    sop.setAttribute("class","sop")
+     sop.innerText=elem.strikedoffprice;
+
+    var dct=document.createElement("p");
+     dct.innerText=elem.discount;
+     var box1=document.createElement("div");
+     box1.setAttribute("id","box1");
+     box1.append(sop,dct);
+
+    var nme=document.createElement("p");
+    nme.setAttribute("class","nme")
+    nme.addEventListener("click", function(){
+        disc(elem)
+    })
+    nme.style.cursor="pointer"
+     nme.innerText=elem.name;
+    
+     box.append(img,box2,box1,nme);
+    
+    document.querySelector("#bottom").append(box);
+}
+);
+
+
+
+function disc(elem){
+    Discription.push(elem);
+    console.log(elem)
+    localStorage.setItem("discription",JSON.stringify(Discription))
+}
