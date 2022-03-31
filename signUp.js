a6f5b724f10ef099ef740dbc107a3ad56418cccb
Kundan <kd181197@gmail.com>
2022-04-01 00:24:11 +0530

    login and signup page added

---
+++ signUp.js
@@ -0,0 +1,40 @@
+document.querySelector("form").addEventListener("submit", myfunction);
+var signData = JSON.parse(localStorage.getItem("userDetail")) || [];
+function myfunction() {
+    event.preventDefault();
+    // console.log("submit") working
+    var Email = document.querySelector(".mail").value;
+    var Pass = document.querySelector(".password").value;
+    if (check(signData, Email)) {
+        alert("Email is alrady register")
+    }else if(Email ==""&&Pass == ""){
+        alert("Invalid Credential")
+    }
+     else {
+        var obj = {
+            email: Email,
+            pass: Pass
+        }
+        signData.push(obj);
+        localStorage.setItem("userDetail", JSON.stringify(signData));
+        document.querySelector(".mail").value = "";
+        document.querySelector(".password").value = ""
+        alert("Your Account has been created")
+        window.location.href = "login.html"
+    }
+
+
+}
+function check(data, target) {
+    if(data.length==0){
+        return false;
+    }
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].email == target) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
